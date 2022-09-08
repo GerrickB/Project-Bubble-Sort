6a5fb4dd07a9e1133c8cdb0acc1ae81f28cb9709
Gerrick B <lsgenether@gmail.com>
2022-09-08 03:46:09 -0700

    Implement bubble_sort draft code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,2 +1,19 @@
 def bubble_sort(array)
+    #array_length = array.length
+    swapped = true
+    print swapped
+    until swapped == false do
+        swapped = false
+        array.each_with_index do |number, index|
+            if array[index - 1] > array[index]
+                bigger = array[index - 1]
+                smaller = number
+                array[index - 1] = smaller
+                array[index] = bigger
+                swapped = true
+            end
+        end
+    end
 end
+
+print bubble_sort([4,3,78,2,0,2])
