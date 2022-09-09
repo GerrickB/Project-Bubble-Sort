a6b9b33f107f0111ea123e3bff938a57fc81f347
Gerrick B <lsgenether@gmail.com>
2022-09-08 23:13:08 -0700

    Implement optimized bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,43 @@
 def bubble_sort(array)
-    #array_length = array.length
+    array_length = array.length
     swapped = true
-    print swapped
     until swapped == false do
         swapped = false
-        array.each_with_index do |number, index|
-            if array[index - 1] > array[index]
-                bigger = array[index - 1]
-                smaller = number
-                array[index - 1] = smaller
-                array[index] = bigger
+        for i in 1..array_length - 1
+            if array[i - 1] > array[i]
+                #swap
+                bigger = array[i - 1]
+                smaller = array[i]
+                array[i - 1] = smaller
+                array[i] = bigger
                 swapped = true
+                puts print array
             end
+        end
+        array_length -= 1
+        puts 'end loop'
+    end
+    print "Final #{array}"
+end
+
+bubble_sort([4,3,78,2,0,2])
+
+#draft code
+=begin
+array = [4,3,78,2,0,2]
+swapped = true
+until swapped == false do
+    swapped = false
+    array.each_with_index do |number, index|
+        if array[index - 1] > array[index]
+            bigger = array[index - 1]
+            smaller = number
+            array[index - 1] = smaller
+            array[index] = bigger
+            swapped = true
+            puts swapped
         end
     end
 end
+=end
 
-print bubble_sort([4,3,78,2,0,2])
